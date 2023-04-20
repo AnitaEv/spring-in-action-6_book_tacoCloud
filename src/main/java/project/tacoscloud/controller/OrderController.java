63751b8972f1d0baa486fbfbd8b000ae5bd1f291
AnitaEv <nitka990@gmail.com>
2023-04-20 17:53:43 +0300

    init comment2

--- src/main/java/project/tacoscloud/controller/OrderController.java
+++ src/main/java/project/tacoscloud/controller/OrderController.java
@@ -3,10 +3,15 @@ package project.tacoscloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import project.tacoscloud.TacoOrder;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -14,12 +19,17 @@ import project.tacoscloud.TacoOrder;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    @GetMapping("/current")
     public String orderForm() {
         return "orderForm.html";
     }
 
-    public String processOrder(TacoOrder order,
+    @PostMapping
+    public String processOrder(@Valid TacoOrder order, Errors errors,
                                SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
 
